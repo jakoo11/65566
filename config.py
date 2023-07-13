31506ce9600948a0fc870c7410decaf803ade810
jakoo11 <79021624+jakoo11@users.noreply.github.com>
2023-07-13 18:39:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "")
-START_IMG = getenv("START_IMG", "")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/1860b3cdf99b31e50dab5.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/1860b3cdf99b31e50dab5.jpg")
 
 SESSION = getenv("SESSION", None)
 
@@ -24,4 +24,4 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/mvhmed")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5656828413").split()))
 
 
-FAILED = ""
+FAILED = "https://telegra.ph/file/1860b3cdf99b31e50dab5.jpg"
